cb6e43cfc1e6b12263746a2e51a29ef7034ff631
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-08-08 12:38:25 +0700

    feat: setup all color palletes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,41 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         thesignature: ["var(--font-thesignature)"],
         unbounded: ["var(--font-unbounded)"],
         albertsans: ["var(--font-albertsans)"],
+      },
+      colors: {
+        border: "rgba(var(--border))",
+        ring: "rgba(var(--ring))",
+        background: "rgba(var(--background))",
+        foreground: "rgba(var(--foreground))",
+        primary: {
+          DEFAULT: "rgba(var(--primary))",
+          foreground: "rgba(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "rgba(var(--secondary))",
+          foreground: "rgba(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "rgba(var(--muted))",
+          foreground: "rgba(var(--muted-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
