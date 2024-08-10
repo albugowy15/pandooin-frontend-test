b481f737e68c72a8a796f736f76381c48c17f36b
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-08-10 14:44:44 +0700

    feat: store navigations config

---
+++ src/configs/navigation.ts
@@ -0,0 +1,22 @@
+export type Navigation = {
+  title: string;
+  href: string;
+};
+export const navigations: Navigation[] = [
+  {
+    title: "Homepage",
+    href: "/",
+  },
+  {
+    title: "Customize Your Trip",
+    href: "#discover-tailored-experiences",
+  },
+  {
+    title: "Destination",
+    href: "#destinations",
+  },
+  {
+    title: "Article",
+    href: "#articles",
+  },
+];
